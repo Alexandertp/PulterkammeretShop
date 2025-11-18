8893d191b560fd1a909125c5340dc8cd5006fe0b
KMS1150 <futty1155@gmail.com>
2025-11-18 12:11:14 +0100

    Vare Constructer

---
+++ PulterkammeretShop/PulterkammeretShop/Models/Vare.cs
@@ -0,0 +1,28 @@
+namespace PulterkammeretShop.Models;
+
+public class Vare
+{
+    /// <summary>
+    /// Constructer for vores Vare klasse
+    ///
+    /// skrevet af Kasper Sørensen.
+    /// </summary>
+    public Vare()
+    {
+        int    id;
+        string name;
+        double pris;
+        string beskrivelse;
+        int    antal;
+        string kategori;
+        string forlag;
+
+    }
+
+
+}
+
+
+
+
+
