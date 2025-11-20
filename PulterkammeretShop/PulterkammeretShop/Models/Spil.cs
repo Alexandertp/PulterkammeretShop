1ffc18cfc871ac94b01723be09f89631d88dc94f
anne474w <101199118+anne474w@users.noreply.github.com>
2025-11-20 11:30:05 +0100

    rename Vare to Spil

---
+++ PulterkammeretShop/PulterkammeretShop/Models/Spil.cs
@@ -0,0 +1,26 @@
+using System.IO;
+using System;
+using PulterkammeretShop.Models;
+namespace PulterkammeretShop.Models;
+
+public class Spil
+{
+    /// <summary>
+    /// Constructer for vores Vare klasse
+    ///
+    /// skrevet af Kasper Sørensen.
+    /// </summary>
+    public int id { get; set; }
+    public string navn { get; set; }
+    public double pris { get; set; }
+    public Spil(int id, string navn, double pris)
+    {
+        this.id = id;
+        this.navn = navn;
+        this.pris = pris;
+    }
+    public string beskrivelse()
+    {
+        return "Spil ID: " + id + ", Navn: " + navn + ", Pris: " + pris;
+    }
+}
