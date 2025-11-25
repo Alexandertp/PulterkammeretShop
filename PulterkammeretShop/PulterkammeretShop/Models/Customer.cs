e7d0d11359b0e600116f7193be211fbc14e18951
anne474w <101199118+anne474w@users.noreply.github.com>
2025-11-25 13:56:25 +0100

    Add members to Customer

--- PulterkammeretShop/PulterkammeretShop/Models/Customer.cs
+++ PulterkammeretShop/PulterkammeretShop/Models/Customer.cs
@@ -4,17 +4,21 @@ namespace PulterkammeretShop.Models
 {
     public class Customer : Account
     {
+        public int PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string PaymentInfo { get; set; }
+
         /// <summary>
         /// Skrevet af Oliver Frølund
         /// </summary>
         public Customer(int Id, string Name, string Password, int PhoneNumber, string Address, string PaymentInfo)
         {
-            int id;
-            string name;
-            string password;
-            int phoneNumber;
-            string address;
-            string paymentInfo;
+            int id = Id;
+            string name = Name;
+            string password = Password;
+            int phoneNumber = PhoneNumber;
+            string address = Address;
+            string paymentInfo = PaymentInfo;
         }
         List <Ordre> ordre = new List<Ordre>();
     }
